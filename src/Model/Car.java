6dde4019435d1325a9bbc42019a6a42371d5a1c6
jojooah <my_sg1@naver.com>
2022-02-12 12:17:13 +0900

    A 개발자 commit

--- src/Model/Car.java
+++ src/Model/Car.java
@@ -3,6 +3,7 @@ package Model;
 public class Car {
     private int speed;
     private String name;
+    private int weight;
 
     public int getSpeed() {
         return speed;
@@ -18,5 +19,13 @@ public class Car {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public void setWeight(int weight) {
+        this.weight = weight;
     }
 }
