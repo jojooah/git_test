236958b97a62e92965fec71cbea79d193a5e0c8b
jojooah <my_sg1@naver.com>
2022-02-12 12:11:26 +0900

    A 개발자 commit

---
+++ src/Model/Car.java
@@ -0,0 +1,22 @@
+package Model;
+
+public class Car {
+    private int speed;
+    private String name;
+
+    public int getSpeed() {
+        return speed;
+    }
+
+    public void setSpeed(int speed) {
+        this.speed = speed;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
